e092dbd632f5e7351131fe7a40598875dd681ae9
consid <consid@SPB26SRV.etm.corp>
2022-07-26 18:57:12 +0300

    wms

---
+++ main.js
@@ -0,0 +1,35 @@
+function getInfo() {
+  fetch('http://localhost:44932/GetInformation/WSS?geoKey=WSS', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'text/plain'
+  },
+  mode: 'no-cors'
+})
+.then(data => console.log(data))
+}
+
+getInfo()
+
+// { "Data": { 
+//   "WrkStation": "ВСЕ РАБОЧИЕ МЕСТА",
+//   "State":"Занято",
+//   "Tasks": { 
+//     "Task": { 
+//       "TaskType": "ОТБОР",
+//       "Priority": "99", 
+//       "TaskStaus":"НОВОЕ",
+//       "PrdCode": "2081482",
+//       "PrdDesc": "м ВВГнгАLSLTx4х1.5(N",
+//       "Amount":"1987",
+//       "LotFrom":"08D/1799",
+//       "LotTo":"BUXT/1799.2",
+//       "BigNum":"2563" 
+//     }
+//   },
+//   Total": {
+//     "TotTasks":"2",
+//     "WrkTasks":"2",
+//     "NewTasks":"0"
+//   }
+// }
